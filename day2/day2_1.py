fc1afe461b8bd9a5b9b618fa998db312f6f9a906
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2024-12-02 18:54:25 +0000

    Finished day 2

--- day2/day2_1.py
+++ day2/day2_1.py
@@ -1,31 +1,39 @@
-total = 0
-with open("input2.txt", "r") as f:
+safe = 0
+with open("input_2.txt", "r") as f:
     for line in f:
-        ok = True
         numbers = line.strip().split(" ")
         numbers = [int(x) for x in numbers]
-        difference = numbers[1] - numbers[0]
-        if difference == 0:
-            continue
-        elif difference < 0:
-            ascending = True
-        elif difference > 0:
-            ascending = False
-        for i in range(1, len(numbers)):
-            separation = numbers[i - 1] - numbers[i]
-            if separation == 0:
-                ok = False
-                break
-            if ascending:
-                if separation < -3:
+        copied_numbers = numbers.copy()
+        for remove_index in range(len(copied_numbers)):
+            ok = True
+            numbers = [x for i, x in enumerate(copied_numbers) if i != remove_index]
+            if len(set(numbers)) == len(numbers):
+                sorted_numbers = sorted(numbers)
+                print(numbers)
+                print(sorted_numbers)
+                ascending = sorted_numbers == numbers
+                print(ascending)
+                sorted_numbers.reverse()
+                descending = sorted_numbers == numbers
+                print(descending)
+                print("-----")
+                if ascending:
+                    for i in range(1, len(numbers)):
+                        difference = numbers[i] - numbers[i-1]
+                        if difference > 3:
+                            ok = False
+                            break
+                elif descending:
+                    for i in range(1, len(numbers)):
+                        difference = numbers[i-1] - numbers[i]
+                        if difference > 3:
+                            ok = False
+                            break
+                else:
                     ok = False
+                if ok:
+                    safe += 1
                     break
-            else:
-                if separation > 3:
-                    ok = False
-                    break
-        if ok:
-            total += 1
-print(total)
+print(safe)
 
         
